ba62fd90bb2c9adcbe4ce5fbfe455f6532253f8a
Sergio <sergio.alejo@unmsm.edu.pe>
2024-06-04 00:15:00 -0500

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,37 @@
+
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+        // Middleware
+        this.middlewares();
+
+        // Rutas de la aplicación
+        this.routes();
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath, require('../routers/usuarios'))
+    }
+
+    middlewares() {
+        //CORS
+        this.app.use(cors());
+        //lectura y parseo del body recibe lo que se envia
+        this.app.use(express.json());
+        //directorio publico
+        this.app.use(express.static('public'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
+        });
+    }
+}
+
+module.exports = Server;
