cca3e6ac6a18542dec2c7f4e4c86ce378c19cb28
Sergio <sergio.alejo@unmsm.edu.pe>
2024-06-23 11:52:54 -0500

    dd

--- database/config.js
+++ database/config.js
@@ -1,21 +1,19 @@
-//FUNCION PARA LA CONEXION CON LAS BASE DE DATOS 
+//FUNCION PARA LA CONEXION CON LAS BASE DE DATOS
 const mongoose = require('mongoose');
-const dbConnection = async() =>{
+
+const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true
-            //useCreateIndex: true,
-            // useFindAndModify: false
         });
         console.log('Base de datos OnLine');
-
     } catch (error) {
-        console.log(error);
+        console.error('Error al iniciar la Base de datos', error);
         throw new Error('Error al iniciar la Base de datos');
     }
-}
-//para esportar las funciones diversas
+};
+
 module.exports = {
     dbConnection
-}
+};
